11a5194d6e4b0fb48acd48559c2cc1756caf998f
mrdoza <marc.mendoza@gmail.com>
2024-06-20 21:27:58 -0500

    Finished for now added the final logic to the js file added the form input > myLibrary logic the script creates the cards as books are added to the library It's ugly but it works

--- script.js
+++ script.js
@@ -5,19 +5,58 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  addBookToLibrary(this);
 }
+
+//Starter Books
+const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const book2 = new Book("The Lord of the Rings", "J.R.R.T.", 500, true);
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+
+// Test Function
+// function displayBooks() {
+//   for (let i = 0; i < myLibrary.length; i++) {
+//     console.log(myLibrary[i]);
+//   }
+// }
+// displayBooks();
+
+const booksGrid = document.querySelector(".booksGrid");
 
 function displayBooks() {
+  booksGrid.innerHTML = ""; // Clear the grid first
   for (let i = 0; i < myLibrary.length; i++) {
-    console.log(myLibrary[i]);
+    const book = myLibrary[i];
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("bookCard");
+    bookCard.innerHTML = `
+      <ul>
+        <li>Title: ${book.title}</li>
+        <li>Author: ${book.author}</li>
+        <li>Pages: ${book.pages}</li>
+        <li>Read: ${book.read}</li>
+      </ul>
+    `;
+    booksGrid.appendChild(bookCard);
   }
 }
-const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-const book2 = new Book("The Lord of the Rings", "J.R.R.T.", 500, true);
 
+const form = document.querySelector("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const read = document.querySelector("#hasRead").checked;
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  displayBooks();
+  form.reset();
+});
+
+// Initial display
 displayBooks();
