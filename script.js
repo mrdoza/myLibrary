13acf6de53e88d6d58f1cc94ffc8549dae89f723
mrdoza <marc.mendoza@gmail.com>
2024-06-29 22:53:16 -0500

    refactoring script.js
    moved addToLibrary as a method of Book class
    moved displayBooks as a method of Book class
    added a removed button and function

--- script.js
+++ script.js
@@ -1,64 +1,63 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
-
-//Starter Books
-const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-const book2 = new Book("The Lord of the Rings", "J.R.R.T.", 500, true);
-
-function addBookToLibrary(book) {
-  myLibrary.push(book);
-}
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-
-// Test Function
-// function displayBooks() {
-//   for (let i = 0; i < myLibrary.length; i++) {
-//     console.log(myLibrary[i]);
-//   }
-// }
-// displayBooks();
-
+const form = document.querySelector("form");
 const booksGrid = document.querySelector(".booksGrid");
 
-function displayBooks() {
-  booksGrid.innerHTML = ""; // Clear the grid first
-  for (let i = 0; i < myLibrary.length; i++) {
-    const book = myLibrary[i];
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("bookCard");
-    bookCard.innerHTML = `
-      <ul>
-        <li>Title: ${book.title}</li>
-        <li>Author: ${book.author}</li>
-        <li>Pages: ${book.pages}</li>
-        <li>Read: ${book.read}</li>
-      </ul>
-    `;
-    booksGrid.appendChild(bookCard);
-  }
-}
-
-const form = document.querySelector("form");
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   const title = document.querySelector("#title").value;
   const author = document.querySelector("#author").value;
   const pages = document.querySelector("#pages").value;
   const read = document.querySelector("#hasRead").checked;
+  if (!title || !author || !pages) {
+    alert("Please fill in all fields");
+    return;
+  }
   const newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
-  displayBooks();
+  newBook.addBook();
   form.reset();
+  newBook.displayBooks();
 });
 
-// Initial display
-displayBooks();
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 
-//todo: Add a button to each book's display to remove the book from the library.
+  this.addBook = () => {
+    myLibrary.push(this);
+  };
+
+  this.displayBooks = () => {
+    booksGrid.innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+      const book = myLibrary[i];
+      const bookCard = document.createElement("div");
+      bookCard.classList.add("bookCard");
+      bookCard.innerHTML = `
+        <ul>
+          <li>Title: ${book.title}</li>
+          <li>Author: ${book.author}</li>
+          <li>Pages: ${book.pages}</li>
+          <li>Read: ${book.read}</li>
+        </ul>
+        <button onclick="removeBook(${i})">Remove</button>
+      `;
+      booksGrid.appendChild(bookCard);
+    }
+  };
+}
+
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  book1.displayBooks();
+}
+
+// starter books
+const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const book2 = new Book("The Lord of the Rings", "J.R.R.T.", 500, true);
+book1.addBook();
+book1.displayBooks();
+book2.addBook();
+book2.displayBooks();
